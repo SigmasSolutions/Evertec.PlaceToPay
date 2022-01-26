b75b32d9f1e2d0e87fa52e2bee600e7f043120df
lposada <lposada81@gmail.com>
2022-01-26 09:59:09 -0500

    Ajustes de environment prd azure

--- Evertec.PlaceToPay.Frontend/src/environments/environment.prod.ts
+++ Evertec.PlaceToPay.Frontend/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   appInsights: {
     instrumentationKey: 'e720d315-e011-476c-971b-143f545da387'
   },
-  baseAPI: 'https://apicuoreprod.azurewebsites.net/api/v1/',
+  baseAPI: 'https://test-evertect-placetopay.azurewebsites.net//api/',
   subscriptionKey: '',
   apiKeyMap: 'AIzaSyD_1bL630os1u4dFjw_92k-iWkLemTHd9U',
   apikeyReCapchat: '6LdGa_QUAAAAAMLShW-o_3rlrq_y53LuxNtHRVV4'
